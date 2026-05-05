4f35a11c38b512ac30e13d1be7bbd1f60064e0d3
Matt Larsen <mattl321@hotmail.com>
2026-05-04 23:18:45 -0700

    Suppress client performance console logs

--- src/RustServerMetrics/HarmonyPatches/BasePlayer_PerformanceReport_Patch.cs
+++ src/RustServerMetrics/HarmonyPatches/BasePlayer_PerformanceReport_Patch.cs
@@ -1,8 +1,4 @@
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
 
 // ReSharper disable InconsistentNaming
 
@@ -11,42 +7,20 @@ namespace RustServerMetrics.HarmonyPatches;
 [HarmonyPatch(typeof(BasePlayer), nameof(BasePlayer.PerformanceReport))]
 public class BasePlayer_PerformanceReport_Patch
 {
-    [HarmonyTranspiler]
-    public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> originalInstructions, ILGenerator iLGenerator)
+    [HarmonyPrefix]
+    public static bool Prefix(BasePlayer __instance, BaseEntity.RPCMessage msg)
     {
-        var skipMetricsLabel = iLGenerator.DefineLabel();
-        var retList = new List<CodeInstruction>(originalInstructions);
+        msg.read.Int32();
+        var memorySystem = msg.read.Int32();
+        var fps = msg.read.Float();
+        msg.read.Int32();
+        msg.read.Bit();
 
-        var bitMethodInfo = typeof(BaseEntity.RPCMessage).GetField(nameof(BaseEntity.RPCMessage.read)).FieldType.GetMethod("Bit", Type.EmptyTypes);
-        var insertionIndex = retList.FindIndex(x => x.opcode == OpCodes.Callvirt && Equals(x.operand, bitMethodInfo));
-        if (insertionIndex < 0) throw new Exception("Failed to find the insertion index for BasePlayer_PerformanceReport_Patch");
-        insertionIndex = retList.FindIndex(insertionIndex, x => x.opcode == OpCodes.Stloc_S || x.opcode == OpCodes.Stloc || x.opcode == OpCodes.Stloc_0 || x.opcode == OpCodes.Stloc_1 || x.opcode == OpCodes.Stloc_2 || x.opcode == OpCodes.Stloc_3);
-        if (insertionIndex < 0) throw new Exception("Failed to find the store index for BasePlayer_PerformanceReport_Patch");
-        insertionIndex++;
+        if (MetricsLogger.IsReady)
+        {
+            MetricsLogger.Instance.OnClientPerformanceReport(__instance, memorySystem, fps);
+        }
 
-        var fieldInfo = typeof(SingletonComponent<MetricsLogger>)
-            .GetField(nameof(SingletonComponent<MetricsLogger>.Instance), BindingFlags.Static | BindingFlags.Public);
-
-        var methodInfo = typeof(MetricsLogger)
-            .GetMethod(nameof(MetricsLogger.OnClientPerformanceReport), BindingFlags.Instance | BindingFlags.NonPublic);
-
-        var labels = retList[insertionIndex].labels;
-        retList[insertionIndex].labels = [];
-        retList[insertionIndex].labels.Add(skipMetricsLabel);
-
-        retList.InsertRange(insertionIndex, [
-            new CodeInstruction(OpCodes.Ldsfld, fieldInfo)
-            {
-                labels = labels,
-            },
-            new CodeInstruction(OpCodes.Brfalse_S, skipMetricsLabel),
-            new CodeInstruction(OpCodes.Ldsfld, fieldInfo),
-            new CodeInstruction(OpCodes.Ldarg_0),
-            new CodeInstruction(OpCodes.Ldloc_0),
-            new CodeInstruction(OpCodes.Ldloc_1),
-            new CodeInstruction(OpCodes.Call, methodInfo),
-        ]);
-
-        return retList;
+        return false;
     }
 }
